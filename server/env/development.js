18b869603330c04bf630e9bd3522b7e678fc77ab
Drew Vartanian <Drew.Vartanian@gmail.com>
2015-09-23 14:47:04 -0400

    google OAuth

--- server/env/development.js
+++ server/env/development.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "DATABASE_URI": "mongodb://localhost:27017/fsg-app",
+  "DATABASE_URI": "mongodb://localhost:27017/trfp",
   "SESSION_SECRET": "Optimus Prime is my real dad",
   "TWITTER": {
     "consumerKey": "INSERT_TWITTER_CONSUMER_KEY_HERE",
@@ -12,8 +12,8 @@ module.exports = {
     "callbackURL": "INSERT_FACEBOOK_CALLBACK_HERE"
   },
   "GOOGLE": {
-    "clientID": "INSERT_GOOGLE_CLIENTID_HERE",
-    "clientSecret": "INSERT_GOOGLE_CLIENT_SECRET_HERE",
-    "callbackURL": "INSERT_GOOGLE_CALLBACK_HERE"
+    "clientID": "625747815020-hipejf4qb23qvhjfl5mo474mt8gupi0f.apps.googleusercontent.com",
+    "clientSecret": "iFE58PfrVcl04nlidvegTfAj",
+    "callbackURL": "http://127.0.0.1:3000/auth/google/callback"
   }
 };
